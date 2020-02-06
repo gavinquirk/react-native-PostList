7f583ef6e41a6d0cfd298bd99da600ce3112e869
Gavin Quirk <gavinquirk@gmail.com>
2020-02-06 13:40:26 -0800

    project setup

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default App;
