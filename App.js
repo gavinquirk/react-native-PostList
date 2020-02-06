166afb1846a5a2c6ec41f4f89f98e18f96f717c3
Gavin Quirk <gavinquirk@gmail.com>
2020-02-06 14:14:03 -0800

    successfull axios request and list

--- App.js
+++ App.js
@@ -1,19 +1,70 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, FlatList} from 'react-native';
+import axios from 'axios';
 
-const App = () => {
-  return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
-  );
-};
+import ListItem from './components/ListItem';
+
+class App extends Component {
+  state = {
+    posts: [],
+  };
+
+  // componentDidMount() {
+  //   console.log('Mounted...');
+
+  //   // Retrieve Posts from API
+  //   axios
+  //     .get('https://jsonplaceholder.typicode.com/posts')
+  //     .then(function(response) {
+  //       console.log(response.data[0].id);
+
+  //       this.setState({
+  //         posts: response.data,
+  //       });
+  //     })
+  //     .catch(function(error) {
+  //       console.log(error);
+  //     });
+  // }
+
+  componentDidMount() {
+    console.log('Mounted');
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(res => {
+      const posts = res.data;
+      this.setState({posts});
+    });
+  }
+
+  render() {
+    // const {posts} = this.state;
+    // console.log(this.state.posts);
+
+    let List;
+
+    if (this.state.posts < 1) {
+      console.log('No Posts...');
+      List = <Text>Loading...</Text>;
+    } else {
+      console.log('Posts Exist');
+      List = (
+        <FlatList
+          data={this.state.posts}
+          renderItem={({item}) => <Text>{item.title}</Text>}
+        />
+      );
+    }
+
+    return (
+      <View>
+        {List}
+
+        {/* <FlatList
+          data={this.state.posts}
+          renderItem={({item}) => <ListItem item={item} />}
+        /> */}
+      </View>
+    );
+  }
+}
 
 export default App;
